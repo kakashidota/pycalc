8dcc04eb1c710be8aedae4481ed7e00d74e0a310
Kakashidota <robin.kamo@live.se>
2021-04-21 21:12:24 +0200

    fixed stuff

--- test_index.py
+++ test_index.py
@@ -4,6 +4,8 @@ from index import Calc
 class TestCalc:
     def test_addition(self):
         assert 4 == Calc.add(2, 2)
+        assert 10 == Calc.add(5, 5)
 
     def test_sub(self):
         assert 2 == Calc.sub(4, 2)
+        assert 10 == Calc.sub(20, 10)
