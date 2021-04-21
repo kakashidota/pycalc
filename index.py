68f2f5a1e1ac74af6d75fdeda8dfbc35b3ecc33c
Kakashidota <robin.kamo@live.se>
2021-04-22 00:44:53 +0200

    removed flask connecetion and fixed some stuff

--- index.py
+++ index.py
@@ -1,8 +1,3 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-
 class Calc:
     @staticmethod
     def add(x, y):
@@ -11,27 +6,3 @@ class Calc:
     @staticmethod
     def sub(x, y):
         return x - y
-
-
-@app.route("/")
-def home():
-    return "hello"
-
-
-@app.route("/add")
-def add():
-    number = str(Calc.add(10, 25))
-    return number
-
-
-@app.route("/sub")
-def sub():
-    number = str(Calc.sub(10, 25))
-    return number
-
-
-app.run()
-# py -m pip freeze > requirements.txt // Fixar en req
-# py -m flake8 --statistics //linter
-# py -m pip install flake8 pytest pytest-cov flask// paket som behövs
-# pytest -v --cov kör tester
